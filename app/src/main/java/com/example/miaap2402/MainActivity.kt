fd7a3d0f36e9da786f85bc6a4c512a10d142c123
sh-ashraful <asrafulalam.sh@gmail.com>
2024-08-25 17:29:59 +0600

    Xml design

---
+++ app/src/main/java/com/example/miaap2402/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.miaap2402
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.miaap2402.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.loginBTN.setOnClickListener {
+
+            Toast.makeText(this@MainActivity, "Login Successfully", Toast.LENGTH_SHORT).show()
+
+        }
+
+    }
+}
